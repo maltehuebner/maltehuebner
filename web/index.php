7eac3950e4be312c3a608607ce257d032768ace0
Malte Hübner <maltehuebner@gmx.org>
2017-10-18 18:15:48 +0200

    Insert twig.

--- web/index.php
+++ web/index.php
@@ -4,4 +4,16 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views/',
+));
+
+$app->get('/{slug}', function (\Silex\Application $app, string $slug) {
+    return $app['twig']->render('hello.twig', array(
+        'slug' => $slug,
+    ));
+});
+
 $app->run();
