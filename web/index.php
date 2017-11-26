87f4096f18cf116a0c0b8c1a8fdea6d23bb3b6c7
Malte Hübner <maltehuebner@gmx.org>
2017-11-26 15:43:36 +0100

    This is a better redirection.

--- web/index.php
+++ web/index.php
@@ -18,9 +18,9 @@ $app->get('/', function (\Silex\Application $app) {
 })->bind('index');
 
 $app->get('/{slug}.html', function (\Silex\Application $app, string $slug) {
-    $templateFilename = sprintf('content/%s.html.twig', $slug);
+    $path = sprintf('/%s', $slug);
 
-    return $app['twig']->render($templateFilename);
+    return $app->redirect($path, 301);
 })->bind('legacy-page');
 
 $app->get('/{slug}', function (\Silex\Application $app, string $slug) {
