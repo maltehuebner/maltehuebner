3b5a8efb7e79734f86075b2d8c74dba5bd02381d
Malte Hübner <maltehuebner@gmx.org>
2018-11-28 08:32:26 +0100

    Controller adjustment.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,20 +1,23 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Skills\BikeSkillList;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/{slug}", name="page")
      */
-    public function indesx()
+    public function page(string $slug = 'index', BikeSkillList $skillList): Response
     {
-        echo "LALA";
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+        $templateName = sprintf('content/%s.html.twig', $slug);
+
+        return $this->render($templateName, [
+            'skills' => $skillList
         ]);
     }
 }
